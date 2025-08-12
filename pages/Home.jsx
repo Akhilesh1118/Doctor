d8e952289665dbe0900b145876920e2b2519c0cc
Akhilesh Yadav <159260120+Akhilesh1118@users.noreply.github.com>
2025-08-12 09:24:18 +0530

    Add files via upload

---
+++ pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import Menu from '../components/Menu'
+import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
+
+
+const Home = () => {
+  return (
+    <div>
+         < Header />
+         <Menu /> 
+         <TopDoctors/>
+        <Banner/> 
+    </div>
+  )
+}
+
+export default Home
+// rafce  to create a new file 
